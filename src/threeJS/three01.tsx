14f6a996e047222ff1ebbef7957964d2cd9467e7
Soeun Ryu <soeunl131@gmail.com>
2026-05-26 16:56:35 +0900

    three01 - 주석 수정

--- src/threeJS/three01.tsx
+++ src/threeJS/three01.tsx
@@ -5,14 +5,22 @@ import { Box, Cone, Cylinder, Environment, MeshTransmissionMaterial, OrbitContro
 const Three01 = () => {
 
     /*
+        [ mesh ]
+        - Three.js의 Mesh객체에 해당
+        - 기하학(geometry)과 재질(material)을 조합하여 3D객체 생성
+        <mesh>
+            <boxGeomtry />
+            <meshStandardMaterial />
+        </mesh>
+
         [ primitive ]
-        - 더 일방적인 용도 (Three.js의 모든 객체: 메시, 그룹, 카메라, 라이트 등...)
-        - 자체적으로 Three.js 객체를 포함하며, 일반적으로 자식 요소를 가지지 않음
+        - 더 일반적인 용도 (Three.js의 모든 객체 : 메시, 그룹, 카메라, 라이트 등...)
+        - 자체적으로 Three.js객체를 포함하며, 일반적으로 자식 요소를 가지지 않음
         <primitive object={someThreeJSObject} />
 
         [ group ]
-        - Three,js의 Group 객체에 해당
-        - 다른 3D 객체들을 자식으로 포함할 수 있음
+        - Three.js의 Group객체에 해당
+        - 다른 3D객체들을 자식으로 포함할 수 있음
         <group>
             <mesh />
             <mesh />
